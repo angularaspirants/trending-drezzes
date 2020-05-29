34675b5abcf38d7d9b99bfed8f31a707d073a9a8
Anita <anitashally@gmail.com>
2020-05-29 19:45:47 -0400

    anita: modified app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trending-drezzes';
+  title = 'trending-drezzes-shop';
 }
