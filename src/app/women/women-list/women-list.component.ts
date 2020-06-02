0879639fbd8b7783a8c937f8efda11e1edec7b2e
santhiya <santhi5star@gmail.com>
2020-06-02 18:21:25 -0400

    santhiya:change service

--- src/app/women/women-list/women-list.component.ts
+++ src/app/women/women-list/women-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { WomenService } from 'src/app/shared/women-service';
+import { WomenModel } from 'src/app/shared/women-model';
 
 @Component({
   selector: 'app-women-list',
   templateUrl: './women-list.component.html',
-  styleUrls: ['./women-list.component.css']
+  styleUrls: ['./women-list.component.css'],
+  providers: [WomenService],
 })
 export class WomenListComponent implements OnInit {
-
-  constructor() { }
+womenItems: WomenModel[];
+  constructor(private womenService: WomenService) { }
 
   ngOnInit(): void {
+    this.womenItems = this.womenService.womenItems;
+    console.log(this.womenItems);
   }
 
 }
