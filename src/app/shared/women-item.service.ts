a9ba923ef8106efce821bd1591af2af347fed557
santhiya <santhi5star@gmail.com>
2020-06-05 17:04:02 -0400

    santhiya: addcssstyle

--- src/app/shared/women-item.service.ts
+++ src/app/shared/women-item.service.ts
@@ -1,9 +1,9 @@
 import { WomenItemModel } from './women-item.model';
 
 export class WomenItemService{
-    womenItems: WomenItemModel[] = [new WomenItemModel('T-shirt','$45.99','../../assets/t-shirts.jpg'),
-                                new WomenItemModel('Jeans','$34.00','../../assets/jeanss.jpg'),
-                                new WomenItemModel('Skirt','$70.99','../../assets/skirt.jpg'),
-                                new WomenItemModel('Chudi','$104.39','../../assets/chudi.jpg')
+    womenItems: WomenItemModel[] = [new WomenItemModel('T-shirt','$25.99','../../assets/t-shirts.jpg'),
+                                new WomenItemModel('Jeans','$15.99','../../assets/jeanss.jpg'),
+                                new WomenItemModel('Skirt','$37.99','../../assets/skirt.jpg'),
+                                new WomenItemModel('Sweater','$49.39','../../assets/sweaters.jpg')
     ];
 }
--- src/app/women/women-list/women-list.component.ts
+++ src/app/women/women-list/women-list.component.ts
@@ -1,24 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { WomenItemService } from 'src/app/shared/women-item.service';
 import { WomenItemModel } from 'src/app/shared/women-item.model';
+import { WomenSizeService } from 'src/app/shared/women-size.service';
 
 @Component({
   selector: 'app-women-list',
   templateUrl: './women-list.component.html',
   styleUrls: ['./women-list.component.css'],
-  providers: [WomenItemService],
+  providers: [WomenItemService, WomenSizeService],
 })
 export class WomenListComponent implements OnInit {
 womenItems = [new WomenItemModel('shirt','$45','../../assets/t-shirt.jpg')];
 totalPrice: number;
 quantity: number;
-  constructor(private womenItemService: WomenItemService) { }
+  constructor(private womenItemService: WomenItemService, private womenSizeService: WomenSizeService) { }
 
   ngOnInit(): void {
     this.womenItems = this.womenItemService.womenItems;
-    console.log(this.womenItems);
+  }
+  // onKeyUp(event: any){
+  //   this.totalPrice = event.target.value * 45.99;
+  // }
+  getSize(type: string){
+    return this.womenSizeService.getSize(type);
+
   }
   onKeyUp(event: any){
-    this.totalPrice = event.target.value * 45.99;
+    this.totalPrice = event.target.value * 11;
   }
 }
